80dca711c85a5a35210be36eb5c6eb8b4b3fde89
ipadsir4 <yee@haw.com>
2026-03-30 14:20:29 +0000

    hdhdhdhdjdnd

--- uwu/shell/req/main.c
+++ uwu/shell/req/main.c
@@ -27,7 +27,7 @@ void	shell_print(char *buf)
 	ft_putchar('|');
 	if (buf && buf[0])
 		add_history(buf);
-	else
+	else if (!buf)
 		ft_putstr("NULL");
 	ft_putchar('\n');
 	free(buf);
@@ -35,8 +35,15 @@ void	shell_print(char *buf)
 
 void	me_handle_signals(int signo)
 {
-	if i()
-		;
+	muh_number = signo;
+	if (muh_number == SIGQUIT)
+		ft_putstr("\nsig quit received\n");
+	else if (muh_number == SIGINT)
+		ft_putstr("\nsig int received\n");
+	rl_on_new_line();
+	rl_replace_line("", 1);
+	rl_redisplay();
+	muh_number = 0;
 }
 
 void	signal_init(struct sigaction *handler)
@@ -48,19 +55,34 @@ void	signal_init(struct sigaction *handler)
 	sigaddset(&mask, SIGQUIT);//one of these literally handles sigquit bruh
 	handler[0].sa_mask = mask;
 	handler[1].sa_mask = mask;
+	handler[0].sa_handler = me_handle_signals;
+	handler[1].sa_handler = me_handle_signals;
 }
 
 int main(void)
 {
-	char *buf;
+	char				*buf;
 	struct sigaction	handler[2];
+	struct sigaction	old[2];
 
 	signal_init(handler);
+	rl_catch_signals = 0;
+	sigaction(SIGINT, &handler[0], &old[0]);
+	sigaction(SIGQUIT, &handler[1], &old[1]);
 	muh_number = 0;
-	while (muh_number++ < 10)
+	while (1)
 	{
 		buf = readline("I am a shell%");
-		shell_print(buf);
+		if (muh_number)
+			free(buf);
+		else
+			shell_print(buf);
+		if (!muh_number && !buf)
+		{
+			ft_putstr("exiting now\n");
+			exit(0);
+		}
+		muh_number = 0;
 	}
 	(void) buf;
 }
