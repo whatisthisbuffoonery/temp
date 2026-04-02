75b114062e27578e0e0544803733331f70fa63a3
Darren Thoo <dthoo@c1r3s1.42singapore.sg>
2026-04-02 23:26:35 +0800

    ljkfhk

--- uwu/shell/main.c
+++ uwu/shell/main.c
@@ -24,16 +24,20 @@ cmd: the cmd name, actually I should call execute() regardless
 io[2]: two fds, debating to not store pipe relationships in node. also dont init pipes on false branch
 */
 
-pid_t	fork_wrapper(t_cmd **cmd, char *input)
+int	syntax(t_cmd **cmd, char *input, int *last)
 {
 	pid_t	ret;
+	int		n;
+
 	if (input && input[0])
 		ret = fork();
 	else
-		return (0);//0 in main means no op, do not update last exit, init exit to 0
+		return (0);//no op, dun change exit
 	if (!ret)
 		good_syntax(cmd, src);//this is a child
-	return (ret);
+	child_wait(ret);
+	if (
+	return ();
 }
 
 int	main(int c, char **v, char **e)
@@ -55,10 +59,8 @@ int	main(int c, char **v, char **e)
 		if (input[0])
 			add_history(input);
 		//LOCK SYNTAX CHECK BEHIND A FORK OMG then exit 1/cmd exit
-		fork_result = fork_wrapper(&cmd, input);//tokenise and not run if extra operators
+		if (syntax(&cmd, input, &last));//tokenise and not run if extra operators
 		//"bash: fork: Resource temporarily unavailable"//DO NOT EXIT ON ERR, YOU ARE THE SHELL
-		if (fork_result > 0)
-			chew_list(cmd, fork_result);//wait in here, decide from there
 		clean_cmd(&cmd);
 //		if (errno)
 //			return (1);//debatable exit code, wait, exit().
