c23e616af20b9605afacc86f0f0f84256b4d5f2b
WSP <darrenthoo@gmail.com>
2026-04-21 10:51:03 +0800

    philo

---
+++ uwu/philo/main.c
@@ -0,0 +1,18 @@
+#include "thing"
+
+void	print_thing(t_philo *philo)
+{
+	static //
+	//will get its own timestamp
+
+int	args(int c, char **v)
+{
+	if (c != 5 && c != 6)
+		return (1);
+}
+
+//if time to eat == time to die, they will die immediately
+int	main(int c, char **v)
+{
+	if (args(c, v))
+		return (1);
