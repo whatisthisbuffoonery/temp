efacb07786229ee6232f90e74ff8e27b9dfaba07
wslYEEEE <darrenthoo@gmail.com>
2026-07-23 15:15:14 +0800

    bweh

--- uwu/philo/retry/print.c
+++ uwu/philo/retry/print.c
@@ -1,54 +1,5 @@
 #include "h_philo.h"
 
-void	timeval_find_max(struct timeval *max, struct timeval *test)//new libft func
-{
-	test->tv_sec = -1;
-	test->tv_usec = -1;
-	if (test->tv_sec > 0)
-		max->tv_sec = -1;
-	else
-	{
-		max->tv_sec = 1;
-		max->tv_sec <<= (sizeof(max->tv_sec) * CHAR_BIT) - 1;
-		max->tv_sec = ~max->tv_sec;
-	}
-	if (test->tv_usec > 0)
-		max->tv_usec = -1;
-	else
-	{
-		max->tv_usec = 1;
-		max->tv_usec <<= (sizeof(max->tv_usec) * CHAR_BIT) - 1;
-		max->tv_usec = ~max->tv_usec;
-	}
-}
-
-int	timeval_diff(struct timeval src1, struct timeval src2)
-{
-	static _Thread_local struct timeval	curr;
-	static _Thread_local struct timeval	test;
-	static _Thread_local struct timeval max;
-	static _Thread_local int			carry;
-
-	curr.tv_usec = src1.tv_usec - src2.tv_usec;
-	carry = (src1.tv_usec < src2.tv_usec);
-	curr.tv_sec = (src1.tv_sec - src2.tv_sec) - carry;
-	if (!test.tv_sec)
-		timeval_find_max(&max, &test);
-	if (carry)
-		curr.tv_usec += (1000 * 1000) + 1;
-	if (src1.tv_sec < src2.tv_sec)
-		curr.tv_sec += (max.tv_sec) + (1 - carry);
-	return ((curr.tv_sec * 1000) + (curr.tv_usec / 1000));
-}
-
-int	timeval_diff_atomic(struct timeval src1, _Atomic struct timeval src2)
-{
-	static _Thread_local struct timeval	input;
-
-	input = src2;
-	return (timeval_diff(src1, input));
-}
-
 struct timeval	print_philo(t_philo *philo, t_args *delay, char *msg)
 {
 	static _Thread_local struct timeval	display;
@@ -71,6 +22,29 @@ struct timeval	print_philo(t_philo *philo, t_args *delay, char *msg)
 	}
 	pthread_mutex_unlock(philo->print);
 	return (display);
+}
+
+void	print_philo_two(t_philo *philo, t_args *delay, char *msg)
+{
+	static _Thread_local char			buf[50];
+	static _Thread_local struct timeval	display;
+	static _Thread_local struct timeval	src;
+	static _Thread_local int			ms;
+	static _Thread_local int			i;
+
+	gettimeofday(&display, NULL);
+	src = philo->last_meal;
+	ms = timeval_diff(display, delay->start);
+	i = bufnum(buf, ms, 50);
+	if (i + 1 < 50)
+	{
+		buf[i++] = ' ';
+		i += bufnum(&buf[i], philo->philoid + 1, 50 - i);
+		if (i + 1 < 50)
+			buf[i++] = ' ';
+	}
+	ft_strlcpy(&buf[i], msg, 50 - i);// NEEDS THREAD LOCAL IMPLEMENTATION
+	write(1, buf, i + ft_strlen(msg));
 }
 
 //I will actually make up my mind on cmp funcs after making bad ones fmllll
--- uwu/philo/retry/run.c
+++ uwu/philo/retry/run.c
@@ -1,27 +1,5 @@
 #include "h_philo.h"
 
-void	*philo_table(void *data)
-{
-	t_args	*delay;
-
-	delay = (t_args *) data;
-	while (!delay->startflag)
-		continue ;
-	while (!delay->deathflag
-		&& delay->done != delay->headcount
-		&& delay->startflag > 0)
-	{
-		if ((delay->rule != 2 && delay->table < delay->half)
-			|| (delay->rule == 2 && !delay->table))
-			continue ;
-		delay->rule += 1;
-		delay->table = 0;
-		if (delay->rule >= 3 - !(delay->headcount % 2))
-			delay->rule = 0;
-	}
-	return (NULL);
-}
-
 void	philo_sleep(int duration)
 {
 	static _Thread_local struct timeval	curr;
