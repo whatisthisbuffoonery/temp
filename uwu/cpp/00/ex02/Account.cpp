10915c13f7d8517b7b38c72b35b13379abd93335
wslYEEEE <darrenthoo@gmail.com>
2026-08-19 09:05:34 +0800

    bank

--- uwu/cpp/00/ex02/Account.cpp
+++ uwu/cpp/00/ex02/Account.cpp
@@ -1,35 +1,123 @@
 #include "Account.hpp"
 #include <iostream>
+#include <ctime>
+
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+/*constructor/destructor*/
+
+//this one is private
+//print?
+Account::Account(void) : _accountIndex(_nbAccounts), _amount(0), _nbDeposits(0), _nbWithdrawals(0)
+{
+	_nbAccounts += 1;
+}
 
 Account::Account(int initial_deposit) : _accountIndex(_nbAccounts), _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0)
 {
 	_nbAccounts += 1;
 	_totalAmount += _amount;
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created" << std::endl;
 }
 
 Account::~Account(void)
 {
 	_nbAccounts -= 1;
 	_totalAmount -= _amount;
-	//maybe modify deposit/withdraw count
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";closed" << std::endl;
 }
 
-static int	Account::getNbAccounts(void)
+/*get global trackers*/
+
+int	Account::getNbAccounts(void)
 {
 	return (_nbAccounts);
 }
 
-static int	Account::getTotalAmount(void)
+int	Account::getTotalAmount(void)
 {
 	return (_totalAmount);
 }
 
-static int	Account::getNbDeposits(void)
+int	Account::getNbDeposits(void)
 {
 	return (_totalNbDeposits);
 }
 
-static int	Account::getNbWithdrawals(void)
+int	Account::getNbWithdrawals(void)
 {
 	return (_totalNbWithdrawals);
+}
+
+/*timestamp*/
+
+static int	tm_pad(int src)
+{
+	if (src < 10)
+		std::cout << 0;
+	return (src);
+}
+
+void	Account::_displayTimestamp(void)
+{
+	std::time_t	timestamp = std::time(NULL);
+	std::tm		*sep = std::localtime(&timestamp);
+
+	std::cout << "[" << sep->tm_year + 1900;
+	std::cout << tm_pad(sep->tm_mon + 1);
+	std::cout << tm_pad(sep->tm_mday) << "_";
+	std::cout << tm_pad(sep->tm_hour);
+	std::cout << tm_pad(sep->tm_min);
+	std::cout << tm_pad(sep->tm_sec) << "]" << " ";
+}
+
+/*random shi*/
+
+void	Account::displayAccountsInfos(void)
+{
+	_displayTimestamp();
+	std::cout << "accounts:" << _nbAccounts << ";total:" << _totalAmount << ";deposits:" << _totalNbDeposits << ";withdrawals:" << _totalNbWithdrawals << std::endl;
+}
+
+//negative number??
+void	Account::makeDeposit(int deposit)
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";p_amount:" << _amount << ";deposit:" << deposit;
+	_totalNbDeposits += 1;
+	_nbDeposits += 1;
+	_totalAmount += deposit;
+	_amount += deposit;
+	std::cout << ";amount:" << _amount << ";nb_deposits:" << _nbDeposits << std::endl;
+}
+
+//return value not used
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";p_amount:" << _amount << ";withdrawal:";
+	if (withdrawal > _amount)
+		return ((std::cout << "refused" << std::endl), 0);
+	_totalNbWithdrawals += 1;
+	_nbWithdrawals += 1;
+	_amount -= withdrawal;
+	_totalAmount -= withdrawal;
+	std::cout << withdrawal << ";amount:" << _amount << ";nb_withdrawals:" << _nbWithdrawals << std::endl;
+	return (1);
+}
+
+int		Account::checkAmount(void) const
+{
+	return (_amount);
+}
+
+void	Account::displayStatus(void) const
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";deposits:" << _nbDeposits << ";withdrawals:" << _nbWithdrawals << std::endl;
 }
